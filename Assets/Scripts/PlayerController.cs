b8c1f7d09d594290397dd225c2535b4cc0c1d922
angadxsingh <angadnahal@gmail.com>
2025-09-12 23:34:23 +0530

    Initial Game basic setup
    1. Set up unity engine.
    2. Set up basic boundary walls and player.
    3. Created basic player controller script to move around.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//First player basic movement script to test functionality 
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        float X = Input.GetAxis("Horizontal");  
+        float Y = Input.GetAxis("Vertical");    
+
+        rb.velocity = new Vector2(X, Y) * speed;
+    }
+}
