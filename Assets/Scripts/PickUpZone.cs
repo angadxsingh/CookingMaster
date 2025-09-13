00c5d40b74fe4bd1d55c033de5b9358a21690169
angadxsingh <angadnahal@gmail.com>
2025-09-13 22:22:42 +0530

    Vegetable PickUp and Inventory system added
    1. Added a vegetable on a counter.
    2. Added a zone next to it where the player can pick the veg up from.
    3. Once picked up the veg is displayed using a player inventory.
    4. Currently only building game logic for one player.

---
+++ Assets/Scripts/PickUpZone.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PickUpZone : MonoBehaviour
+{
+    public GameObject pickupPrompt;   
+    public Sprite vegetableSprite;    
+ 
+    private bool playerInZone = false;
+
+    private void Start()
+    {
+        pickupPrompt.SetActive(false);                //using prompt when player in zone to let them pick up
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))                //using a collider and a zone to let player pick up vegetable
+        {
+            playerInZone = true;
+            pickupPrompt.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInZone = false;
+            pickupPrompt.SetActive(false);
+        }
+    }
+
+    private void Update()
+    {
+        if (playerInZone && Input.GetKeyDown(KeyCode.F))
+        {
+            PlayerInventory inventory = FindObjectOfType<PlayerInventory>();     
+            if (inventory != null && inventory.CanPickup())          //checks if any veg is in hand
+            {
+                inventory.PickupVegetable(vegetableSprite);           
+                Debug.Log($"picked up and displayed");
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerInventory.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerInventory : MonoBehaviour
+{
+    public Sprite carriedVegetableSprite = null;
+    public Image carryDisplaySlot;
+
+    private void Start()
+    {
+        carryDisplaySlot.enabled = false;                       //a blank image slot on the top left that will show the carried vegtable
+    }  
+
+    public bool CanPickup()
+    {
+        return carriedVegetableSprite == null;                  //to check if player can pick up a vegetable, currently for 1 vegetable only
+    }
+
+    public void PickupVegetable(Sprite vegetableSprite)
+    {
+        if (CanPickup())                                           //places the vegetable sprite in the blank top left space to show what vegetabe is being carried
+        {
+            carriedVegetableSprite = vegetableSprite;
+            carryDisplaySlot.sprite = carriedVegetableSprite;
+            carryDisplaySlot.enabled = true;
+        }
+    }
+}
